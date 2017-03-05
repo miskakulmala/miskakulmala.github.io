ff2b93cdccd819195c17a9f04da070ea58343e26
miskakulmala <miska.kulmala@aalto.fi>
2017-03-05 16:53:07 +0200

    lal

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,17 @@
-window.onload = function () {
-    $.getJSON("https://kulmalm5.firebaseio.com/.json", function (data) {
-        console.log(data);
+$(document).ready(function(){
+    $.getJSON('https://kulmalm5.firebaseio.com/.json',function(res){
+        $(window).unload(saveSettings);
+        loadSettings();
+        addArticle(res.articles);
     });
-}
+    //function that changes the article_index with +1 and calls for change article
+    $('#next').click('click',function(e) {
+        article_index ++;
+        changeArticle("next");
+    });
+    //function that changes the article_index with -1 and calls for change article
+    $('#previous').click('click',function(e) {
+        article_index --;
+        changeArticle("previous");
+    });
+});
