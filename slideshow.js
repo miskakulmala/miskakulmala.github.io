0ce23767393c6294d669324c4765db33d35728f4
miskakulmala <miska.kulmala@aalto.fi>
2017-03-05 17:20:50 +0200

    samperi

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,25 @@
+var muuttuja = 1;
+
 window.onload = function () {
     'use strict';
     $.getJSON("https://kulmalm5.firebaseio.com/.json", function (data) {
     console.log(data);
 });
 };
+
+function display() {
+    'use strict';
+    $.getJSON("https://kulmalm5.firebaseio.com/.json", function (data) {
+        $('#otsikko').html(data.uutiset[index].otsikko);
+        $('#paivamaara').html(data.uutiset[index].paivamaara);
+        $('#sisalto').html(data.uutiset[index].sisalto);
+    });
+}
+
+function switchA() {
+    'use strict';
+    setInterval(function () {
+        nextSlide();
+    }, 3000);
+}
     
